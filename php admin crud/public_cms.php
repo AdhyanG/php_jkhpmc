fd779ffea75587dbcdc14cab16ff08bdbf09038a
MisHti03 <bhartithakur0186@gmail.com>
2023-04-08 15:22:14 +0530

    navbar2

--- php admin crud/public_cms.php
+++ php admin crud/public_cms.php
@@ -43,18 +43,23 @@ if(isset($_GET['delete']))
       </div>
 
       <nav id="navbar" class="nav-menu navbar">
-  <ul>
-    <li><a href="home.php" class="nav-link scrollto"><i class="bx bx-envelope"></i> <span>Home</span></a></li>
-    <li><a href="about_us_cms.php" class="nav-link scrollto "><i class="bx bx-user"></i> <span>About us</span></a></li>
-    <li><a href="mission_cms.php" class="nav-link scrollto"><i class="bx bx-file-blank"></i> <span>Mission</span></a></li>
-    <li><a href="objectives_cms.php" class="nav-link scrollto"><i class="bx bx-book-content"></i> <span>Objectives</span></a></li>
-    <li><a href="tenders_cms.php" class="nav-link scrollto"><i class="bx bx-envelope"></i> <span>Tenders</span></a></li>
-    <li><a href="product_cms.php" class="nav-link scrollto"><i class="bx bx-envelope"></i> <span>Products</span></a></li>
-    <li><a href="public_cms.php" class="nav-link scrollto"><i class="bx bx-envelope"></i> <span>Public Notices</span></a></li>
-    <li><a href="expp_cms.php" class="nav-link scrollto"><i class="bx bx-envelope"></i> <span>Expression of Interest</span></a></li>
-    <li><a href="cover_image_cms.php" class="nav-link scrollto"><i class="bx bx-server"></i> <span>Cover Images</span></a></li>
-    <li><a href="committee_cms.php" class="nav-link scrollto"><i class="bx bx-envelope"></i> <span>Committee Of JKHPMC</span></a></li>
-  </ul>
+ <?php
+  $page = basename($_SERVER['PHP_SELF']);
+?>
+
+<ul class="nav-menu">
+  <li><a href="home.php" class="nav-link <?php if ($page == 'home.php') { echo 'active'; } ?>"><i class="bx bx-envelope"></i> <span>Home</span></a></li>
+  <li><a href="about_us_cms.php" class="nav-link <?php if ($page == 'about_us_cms.php') { echo 'active'; } ?>"><i class="bx bx-user"></i> <span>About us</span></a></li>
+  <li><a href="mission_cms.php" class="nav-link <?php if ($page == 'mission_cms.php') { echo 'active'; } ?>"><i class="bx bx-file-blank"></i> <span>Mission</span></a></li>
+  <li><a href="objectives_cms.php" class="nav-link <?php if ($page == 'objectives_cms.php') { echo 'active'; } ?>"><i class="bx bx-book-content"></i> <span>Objectives</span></a></li>
+  <li><a href="tenders_cms.php" class="nav-link <?php if ($page == 'tenders_cms.php') { echo 'active'; } ?>"><i class="bx bx-envelope"></i> <span>Tenders</span></a></li>
+  <li><a href="product_cms.php" class="nav-link <?php if ($page == 'product_cms.php') { echo 'active'; } ?>"><i class="bx bx-envelope"></i> <span>Products</span></a></li>
+  <li><a href="public_cms.php" class="nav-link <?php if ($page == 'public_cms.php') { echo 'active'; } ?>"><i class="bx bx-envelope"></i> <span>Public Notices</span></a></li>
+  <li><a href="expp_cms.php" class="nav-link <?php if ($page == 'expp_cms.php') { echo 'active'; } ?>"><i class="bx bx-envelope"></i> <span>Expression of Interest</span></a></li>
+  <li><a href="cover_image_cms.php" class="nav-link <?php if ($page == 'cover_image_cms.php') { echo 'active'; } ?>"><i class="bx bx-server"></i> <span>Cover Images</span></a></li>
+  <li><a href="committee_cms.php" class="nav-link <?php if ($page == 'committee_cms.php') { echo 'active'; } ?>"><i class="bx bx-envelope"></i> <span>Committee Of JKHPMC</span></a></li>
+</ul>
+
 </nav>
 <!-- .nav-menu -->
     </div>
